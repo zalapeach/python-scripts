8420713816f5411283d3d087f89d29b7eed34ce8
Juan Aguilar <Juan.Aguilar@epam.com>
2026-02-05 08:07:10 -0600

    scripts of today February 5th 2026

---
+++ 16_spathiphyllum.py
@@ -0,0 +1,9 @@
+text = input("Try to wrote Spathiphyllum: ")
+if text == "Spathiphyllum":
+  print("Yes, Spathiphyllum is the best plant ever!")
+elif text == "spathiphyllum":
+  print("No, I want a big Spathiphyllum!")
+elif text == "SPATHIPHYLLUM":
+  print("HEY, DO NOT YELL TO ME!!!")
+else:
+  print("Spathiphyllum!, not", text + "!")
